37f7f62a2d15ff655a2390c1dda991f590719524
DamienDrm <dam.draime@gmail.com>
2026-04-01 20:07:51 -0400

    Enh.: Leetcode-30

--- substrings-with-concatenation-of-all-words.py
+++ substrings-with-concatenation-of-all-words.py
@@ -3,36 +3,50 @@
 # You are given a string s and an array of strings words. All the strings of words are of the same length.
 
 
-PERMUTATIONS = set()
+from copy import deepcopy
 
 
 def findSubstring(s: str, words: list[str]) -> list[int]:
-    get_permutations(words)
-    perm_len = len(next(iter(PERMUTATIONS)))
+    word_len = len(words[0])
+    perm_len = word_len * len(words)
+    words = get_words_freq(words)
     n = len(s)
     idx = []
-    i = 0
-    while i <= n - perm_len:
-        if s[i : i + perm_len] in PERMUTATIONS:
-            idx.append(i)
-        i += 1
+
+    for j in range(word_len):
+        i = j
+        seen_words = deepcopy(words)
+        counter = 0
+        while i <= n - perm_len:
+            while (w := s[i + counter : i + counter + word_len]) in words and counter < perm_len:
+                if seen_words[w] == 0:
+                    offset = 0
+                    while (pop := s[i + offset : i + offset + word_len]) != w:
+                        seen_words[pop] += 1
+                        offset += word_len
+                    i += offset + word_len
+                    counter -= offset
+                    continue
+                seen_words[w] -= 1
+                counter += word_len
+            if counter == perm_len:
+                idx.append(i)
+                seen_words[s[i : i + word_len]] += 1
+                counter -= word_len
+                i += word_len
+            else:
+                i += word_len + counter
+                seen_words = deepcopy(words)
+                counter = 0
+
     return idx
 
 
-def get_permutations(words: list[str]) -> None:
-    global PERMUTATIONS
+def get_words_freq(words: list[str]) -> dict[str, int]:
+    w = {}
     for word in words:
-        generate_permutations(word)
-    PERMUTATIONS = set("".join(p) for p in PERMUTATIONS)
-
-
-def generate_permutations(word: str) -> None:
-    global PERMUTATIONS
-    if len(PERMUTATIONS) == 0:
-        PERMUTATIONS = {(word,)}
-        return
-    perms = []
-    for permutation in PERMUTATIONS:
-        for i in range(len(permutation) + 1):
-            perms.append(permutation[:i] + (word,) + permutation[i:])
-    PERMUTATIONS = perms
+        if word in w:
+            w[word] += 1
+        else:
+            w[word] = 1
+    return w
