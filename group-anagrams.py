3f5bff7a5ae4371629bd621a33ecab41d77bd5b5
Dam.Draime <dam.draime@gmail.com>
2026-04-20 19:39:49 -0400

    Enh.: Leetcode-49

--- group-anagrams.py
+++ group-anagrams.py
@@ -3,27 +3,13 @@
 # Given an array of strings strs, group the anagrams together. You can return the answer in any order.
 
 
+from collections import defaultdict
+
+
 def groupAnagrams(strs: list[str]) -> list[list[str]]:
-    groups = []
-    group__representatives = []
+    groups = defaultdict(list)
     for x in strs:
-        x_s = sorted(x)
+        key = "".join(sorted(x))
+        groups[key].append(x)
 
-        for i, g_repr in enumerate(group__representatives):
-            if are_anagrams(x_s, g_repr):
-                groups[i].append(x)
-                break
-        else:
-            groups.append([x])
-            group__representatives.append(x_s)
-
-    return groups
-
-
-def are_anagrams(a: list[str], b: list[str]) -> bool:
-    if len(a) != len(b):
-        return False
-    for x, y in zip(a, b):
-        if x != y:
-            return False
-    return True
+    return list(groups.values())
