59a46da47a7eb209168b0a57fc90932225f9de0b
Dam.Draime <dam.draime@gmail.com>
2026-04-20 19:39:05 -0400

    Enh.: Leetcode-49

--- group-anagrams.py
+++ group-anagrams.py
@@ -5,35 +5,25 @@
 
 def groupAnagrams(strs: list[str]) -> list[list[str]]:
     groups = []
-    fds = []
+    group__representatives = []
     for x in strs:
-        x_fd = get_freq_dict(x)
+        x_s = sorted(x)
 
-        for i, group_fd in enumerate(fds):
-            if are_anagrams(x_fd, group_fd):
+        for i, g_repr in enumerate(group__representatives):
+            if are_anagrams(x_s, g_repr):
                 groups[i].append(x)
                 break
         else:
             groups.append([x])
-            fds.append(x_fd)
+            group__representatives.append(x_s)
 
     return groups
 
 
-def get_freq_dict(x: str) -> dict[str, int]:
-    fd = {}
-    for s in x:
-        if s in fd:
-            fd[s] += 1
-        else:
-            fd[s] = 1
-    return fd
-
-
-def are_anagrams(a_fd: dict[str, int], b_fd: dict[str, int]) -> bool:
-    if len(a_fd) != len(b_fd):
+def are_anagrams(a: list[str], b: list[str]) -> bool:
+    if len(a) != len(b):
         return False
-    for k, v in a_fd.items():
-        if b_fd.get(k, None) != v:
+    for x, y in zip(a, b):
+        if x != y:
             return False
     return True
