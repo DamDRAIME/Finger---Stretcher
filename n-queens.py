0b8bd8a94525f8658dda1516744d11b8aed99e71
DamienDrm <dam.draime@gmail.com>
2026-04-27 21:17:52 -0400

    Enh.: Leetcode-51

--- n-queens.py
+++ n-queens.py
@@ -4,55 +4,46 @@
 # other. Given an integer n, return all distinct solutions to the n-queens puzzle. You may return the answer in any
 # order.
 
-from copy import deepcopy
 from collections import defaultdict
 
-solutions = []
 n = 0
+solutions = []
 
 
 def solveNQueens(n_queens: int) -> list[list[str]]:
     global n
     global solutions
     n = n_queens
-    row_constraints = defaultdict(int)
-    col_constraints = defaultdict(int)
-    diag_constraints = defaultdict(int)
-    idiag_constraints = defaultdict(int)
-    board = [["?"] * n for _ in range(n)]
-    solve(n, board, row_constraints, col_constraints, diag_constraints, idiag_constraints)
+    col_constraints = defaultdict(bool)
+    diag_constraints = defaultdict(bool)
+    idiag_constraints = defaultdict(bool)
+    board = [["."] * n for _ in range(n)]
+    solve(n, board, col_constraints, diag_constraints, idiag_constraints)
     return solutions
 
 
-def solve(n, board, r_c, c_c, d_c, id_c) -> None:
+def solve(
+    n_queens: int, board: list[list[str]], c_c: dict[int, bool], d_c: dict[int, bool], id_c: dict[int, bool]
+) -> None:
     if n == 0:
         global solutions
-        solutions.append(["".join(row).replace("?", ".") for row in board])
+        solutions.append(["".join(row) for row in board])
         return
 
-    for y, row in enumerate(board):
-        for x, el in enumerate(row):
-            if el == "?":
-                if is_valid_pos(y, x, r_c, c_c, d_c, id_c):
-                    temp_board = deepcopy(board)
-                    temp_board[y][x] = "Q"
-                    solve(n - 1, temp_board, *update_constraints(y, x, r_c, c_c, d_c, id_c))
-                board[y][x] = "."
-
-
-def is_valid_pos(y, x, r_c, c_c, d_c, id_c):
     global n
-    return not (r_c[x] or c_c[y] or d_c[(y - x) + (n - 1)] or id_c[(y - (n - 1 - x)) + (n - 1)])
+    y = n - n_queens
+    for x, el in enumerate(board[y]):
+        if is_valid_pos(y, x, c_c, d_c, id_c):
+            board[y][x] = "Q"
+            c_c[x] = True
+            d_c[(y - x) + (n - 1)] = True
+            id_c[(y - (n - 1 - x)) + (n - 1)] = True
+            solve(n - 1, board, c_c, d_c, id_c)
+            c_c[x] = False
+            d_c[(y - x) + (n - 1)] = False
+            id_c[(y - (n - 1 - x)) + (n - 1)] = False
+            board[y][x] = "."
 
 
-def update_constraints(y, x, r_c, c_c, d_c, id_c):
-    global n
-    r_c = deepcopy(r_c)
-    r_c[x] = 1
-    c_c = deepcopy(c_c)
-    c_c[y] = 1
-    d_c = deepcopy(d_c)
-    d_c[(y - x) + (n - 1)] = 1
-    id_c = deepcopy(id_c)
-    id_c[(y - (n - 1 - x)) + (n - 1)] = 1
-    return r_c, c_c, d_c, id_c
+def is_valid_pos(y: int, x: int, c_c: dict[int, bool], d_c: dict[int, bool], id_c: dict[int, bool]):
+    return not (c_c[x] or d_c[(y - x) + (n - 1)] or id_c[(y - (n - 1 - x)) + (n - 1)])
