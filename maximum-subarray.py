73da587435253b99152e02b69b48cc1f5ede629e
DamienDrm <dam.draime@gmail.com>
2026-04-29 21:36:34 -0400

    Enh.: Leetcode-53

--- maximum-subarray.py
+++ maximum-subarray.py
@@ -5,10 +5,8 @@
 
 def maxSubArray(nums: list[int]) -> int:
     best = nums[0]
-    for i in range(len(nums)):
-        curr_best = 0
-        for x in nums[i:]:
-            curr_best += x
-            if curr_best > best:
-                best = curr_best
+    prev = nums[0]
+    for x in nums[1:]:
+        prev = max(x, prev + x)
+        best = max(best, prev)
     return best
