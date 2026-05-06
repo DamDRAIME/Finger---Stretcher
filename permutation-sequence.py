2ba4002ad9f03349f54c85450e5ed4f87c716ce0
DamienDrm <dam.draime@gmail.com>
2026-05-06 17:33:18 -0400

    Enh.: Leetcode-60

--- permutation-sequence.py
+++ permutation-sequence.py
@@ -4,8 +4,8 @@
 
 
 def getPermutation(n: int, k: int) -> str:
-    perm = list(range(1, n + 1))
-    for _ in range(k - 1):
+    perm, n_iter = smart_init(n, k)
+    for _ in range(n_iter):
         next_permutation(perm, n)
     return "".join(map(str, perm))
 
@@ -24,3 +24,15 @@ def next_permutation(perm: list[int], n: int):
                 k -= 1
             return
     perm.reverse()
+
+
+def smart_init(n: int, k: int) -> tuple[list[int], int]:
+    def factorial(n):
+        if n == 0 or n == 1:
+            return 1
+        return n * factorial(n - 1)
+
+    n_1_fact = factorial(n - 1)
+    start_num = (k // n_1_fact) + int(bool(k % n_1_fact))
+    perm = [start_num] + [x for x in range(1, n + 1) if x != start_num]
+    return perm, k - ((start_num - 1) * n_1_fact) - 1
